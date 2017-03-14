22ad10b264410c469d8bd137758df8cc53d308bf
msneddon <mwsneddon@lbl.gov>
2017-03-14 14:12:17 -0700

    updated example records; updated solr setup script

--- solr4/setup_core.sh
+++ solr4/setup_core.sh
@@ -4,7 +4,12 @@ rm -rf cores/GenomeFeatures
 cp -rv cores/GenomeFeatures_base cores/GenomeFeatures
 
 
-curl "http://localhost:8983/solr/admin/cores?wt=json&indexInfo=false&action=CREATE&name=GenomeFeatures_prod&instanceDir=%2Fcores%2FGenomeFeatures&dataDir=&config=solrconfig.xml&schema=schema.xml"
+CORE=GenomeFeatures_prod
+curl "http://localhost:8983/solr/admin/cores?wt=json&indexInfo=false&action=CREATE&name=$CORE&instanceDir=%2Fcores%2FGenomeFeatures&dataDir=&config=solrconfig.xml&schema=schema.xml"
 
-# load one record
-curl -H "Content-Type: application/json" 'localhost:8983/solr/GenomeFeatures_prod/update?wt=json' --data "@example_records/genomes/g1.json"
+# load a few records
+curl -H "Content-Type: application/json" "localhost:8983/solr/$CORE/update?wt=json" --data "@example_records/genomes/g1.json"
+curl -H "Content-Type: application/json" "localhost:8983/solr/$CORE/update?wt=json" --data "@example_records/genomes/g2.json"
+
+curl -H "Content-Type: application/json" "localhost:8983/solr/$CORE/update?wt=json" --data "@example_records/features/f1.json"
+curl -H "Content-Type: application/json" "localhost:8983/solr/$CORE/update?wt=json" --data "@example_records/features/f2.json"
